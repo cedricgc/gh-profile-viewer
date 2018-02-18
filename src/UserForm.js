d81ff8ca132d98aa13a7cd58104d4d1b2e731c77
Cedric Charly <cedric.charly@gmail.com>
2018-02-18 12:58:46 -0600

    feat(UserForm.js): have UserForm track username in state

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h2>Input the Github user to display</h2>
           <section>
             <div>
-              <label for="username">
+              <label>
                 <span>Username: </span>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  data-test-id="username"
+                  onChange={this.handleChange}
+                />
               </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                data-test-id="username"
-              />
+              <input type="submit" value="submit" />
             </div>
           </section>
         </form>

26aee297bc6292beec1ad509922c0c47a9d42263
Cedric Charly <cedric.charly@gmail.com>
2018-02-21 23:01:23 -0600

    feat: query GH user info from graphql endpoint

--- src/ProfileView.js
+++ src/ProfileView.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
 class ProfileView extends Component {
   render() {
+    if (this.props.username === '') {
+      return null;
+    }
+
+    const GET_USER = gql`
+      query {
+        user(login: "${this.props.username}") {
+          id
+          name
+          url
+        }
+      }
+    `;
+
     return (
       <div id="gh-profile" data-test-id="profile-container">
         <h2>Github Profile: {this.props.username}</h2>
+        <div>
+          <Query query={GET_USER}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return <div>Loading...</div>;
+              }
+              if (error) {
+                return (
+                  <div>
+                    Error: Unable to load user {`${this.props.username}`}
+                  </div>
+                );
+              }
+
+              return <div>Github ID: {data.user.id}</div>;
+            }}
+          </Query>
+        </div>
       </div>
     );
   }

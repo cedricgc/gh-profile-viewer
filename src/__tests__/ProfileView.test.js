77c958b9c63e9df5916726905ffbc1f848475040
Cedric Charly <cedric.charly@gmail.com>
2018-02-24 17:37:54 -0600

    refactor(ProfileView.js): refactor component to presentational, extract graphql logic
    Switched to using grahql HOC to separate UI logic from networking and to simplify testing.

--- src/__tests__/ProfileView.test.js
+++ src/__tests__/ProfileView.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 
-import ProfileView from '../ProfileView';
+import { ProfileView } from '../ProfileView';
 
-xtest('component matches snapshot', () => {
-  const render = ReactTestRenderer.create(<ProfileView username={'user'} />);
+test('component matches snapshot', () => {
+  const render = ReactTestRenderer.create(
+    <ProfileView
+      username={'user'}
+      data={{ user: { id: 'IDXXXX' }, loading: false, error: false }}
+    />
+  );
 
   const tree = render.toJSON();
   expect(tree).toMatchSnapshot();
